4b15bfff643d8478ce605a5910463e284203c346
Dmitry <muska2203@mail.ru>
2019-09-01 22:53:42 +0300

    Рефакторинг. Вынес алгоритм update всех майнеров в главный класс.

--- src/main/java/com/dreamteam/powerofwar/game/object/GoldMiner.java
+++ src/main/java/com/dreamteam/powerofwar/game/object/GoldMiner.java
@@ -1,12 +1,7 @@
 package com.dreamteam.powerofwar.game.object;
 
-import com.dreamteam.powerofwar.game.Board;
 import com.dreamteam.powerofwar.game.user.User;
 import com.dreamteam.powerofwar.phisics.Units;
-import com.dreamteam.powerofwar.phisics.Vector;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class GoldMiner extends Miner {
 
@@ -15,7 +10,7 @@ public class GoldMiner extends Miner {
     }
 
     @Override
-    public GameObjectType getResource() {
+    public GameObjectType getResourceType() {
         return GameObjectType.GOLD;
     }
 
@@ -47,47 +42,5 @@ public class GoldMiner extends Miner {
     @Override
     public GameObjectType getType() {
         return GameObjectType.GOLD_MINER;
-    }
-
-    @Override
-    public void update(Board board) {
-        if (target != null) {
-            // если дошли до цели
-            if (GameObjectUtils.checkCollision(this, target)) {
-                if (GameObjectType.BASE.equals(target.getType())) {
-                    board.addAction(new BringResourceAction((Base) target, this));
-                } else {
-                    board.addAction(new MineResourceAction((Resource) target, this));
-                }
-                this.target = null;
-            }
-            return;
-        }
-
-        // ищем новую цели для рабочего
-        List<GameObject> mines = board.getGameObjects()
-                    .stream()
-                    .filter(gameObject -> GameObjectType.GOLD.equals(gameObject.getType()))
-                    .collect(Collectors.toList());
-
-
-        List<GameObject> bases = board.getGameObjects()
-                .stream()
-                .filter(gameObject -> GameObjectType.BASE.equals(gameObject.getType()))
-                .collect(Collectors.toList());
-
-        if (this.value < this.getCapacity() && !mines.isEmpty()) {
-            this.target = GameObjectUtils.getNearestObject(this, mines);
-        } else if (this.value > 0 && mines.isEmpty() && !bases.isEmpty()) {
-            this.target = GameObjectUtils.getNearestObject(this, bases);
-        } else if (this.value == this.getCapacity() && !bases.isEmpty()) {
-            this.target = GameObjectUtils.getNearestObject(this, bases);
-        } else {
-            target = null;
-            this.setSpeedVector(new Vector());
-        }
-        if (this.target != null) {
-            this.setSpeedVector(Vector.byTarget(this, target));
-        }
     }
 }
--- src/main/java/com/dreamteam/powerofwar/game/object/Miner.java
+++ src/main/java/com/dreamteam/powerofwar/game/object/Miner.java
@@ -1,7 +1,11 @@
 package com.dreamteam.powerofwar.game.object;
 
+import com.dreamteam.powerofwar.game.Board;
 import com.dreamteam.powerofwar.game.user.User;
 import com.dreamteam.powerofwar.phisics.Vector;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class Miner extends BaseGameObject {
 
@@ -12,7 +16,7 @@ public abstract class Miner extends BaseGameObject {
         super(x, y, new Vector(), user);
     }
 
-    public abstract GameObjectType getResource();
+    public abstract GameObjectType getResourceType();
 
     public abstract int getCapacity();
 
@@ -31,5 +35,47 @@ public abstract class Miner extends BaseGameObject {
 
     public void resetValue() {
         this.value = 0;
+    }
+
+    @Override
+    public void update(Board board) {
+        if (target != null) {
+            // если дошли до цели
+            if (GameObjectUtils.checkCollision(this, target)) {
+                if (GameObjectType.BASE.equals(target.getType())) {
+                    board.addAction(new BringResourceAction((Base) target, this));
+                } else {
+                    board.addAction(new MineResourceAction((Resource) target, this));
+                }
+                this.target = null;
+            }
+            return;
+        }
+
+        // ищем новую цели для рабочего
+        List<GameObject> mines = board.getGameObjects()
+                .stream()
+                .filter(gameObject -> getResourceType().equals(gameObject.getType()))
+                .collect(Collectors.toList());
+
+
+        List<GameObject> bases = board.getGameObjects()
+                .stream()
+                .filter(gameObject -> GameObjectType.BASE.equals(gameObject.getType()))
+                .collect(Collectors.toList());
+
+        if (this.value < this.getCapacity() && !mines.isEmpty()) {
+            this.target = GameObjectUtils.getNearestObject(this, mines);
+        } else if (this.value > 0 && mines.isEmpty() && !bases.isEmpty()) {
+            this.target = GameObjectUtils.getNearestObject(this, bases);
+        } else if (this.value == this.getCapacity() && !bases.isEmpty()) {
+            this.target = GameObjectUtils.getNearestObject(this, bases);
+        } else {
+            target = null;
+            this.setSpeedVector(new Vector());
+        }
+        if (this.target != null) {
+            this.setSpeedVector(Vector.byTarget(this, target));
+        }
     }
 }
